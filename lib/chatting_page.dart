926116cf09893f59dcdb82922e13a0c474d15075
NOGUEN <nhg1113@naver.com>
2022-10-18 17:03:29 +0900

    feat: 채팅 페이지 기반(작성된 것 없음)

---
+++ lib/chatting_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class chattingPage extends StatelessWidget {
+  const chattingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('윤정한'),
+      ),
+      body: Container(),
+      bottomSheet: Container(),
+    );
+  }
+}
